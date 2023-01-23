7216ec7a1d9f1135ffff0f6ae8bb20e19d742272
GUEVARA0331 <jdguevara01@misena.edu.co>
2023-01-23 15:16:01 -0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { /* Default route */
+    /* return view('welcome'); */
+    return "Bienvenido a la página principal.";
+});
+
+Route::get('cursos', function () { /* Route with response text */
+    return "Bienvenido a la página cursos.";
+});
+
+Route::get('cursos/create', function () { /* Route with specific name */
+    return "En esta página podrás crear un curso";
+});
+
+//Route::get('cursos/{curso}', function ($curso) { /* Route with a single parameter */
+//    return "Bienvenido al curso: $curso";
+//});
+
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) { /* Route with optional parameters */
+
+    if($categoria){
+        return "Bienvenido al curso $curso, de la categoría $categoria";
+    }else{
+        return "Bienvenido al curso: $curso";
+    }
+
 });
