753cfb70887d39b9292fd01686c49e7452f8ef94
GUEVARA0331 <jdguevara01@misena.edu.co>
2023-01-25 23:04:30 -0500

    Eloquent

---
+++ app/Models/Curso.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+
+    /// protected $table = 'users'; /* This is used when we want relate the nodel with diferente tables sinde its name */
+}
